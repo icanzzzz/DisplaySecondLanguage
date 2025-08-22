af283699a0a6444ff0c07cdedbae6f9a144909ab
WhiteSky_Bai <2541441004@qq.com>
2025-08-22 18:20:52 +0800

    refine code

--- src/main/java/io/github/icanzzzz/DisplaySecondLanguage/ModClient.java
+++ src/main/java/io/github/icanzzzz/DisplaySecondLanguage/ModClient.java
@@ -3,7 +3,6 @@ package io.github.icanzzzz.DisplaySecondLanguage;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.main.GameConfig;
 import net.minecraft.network.chat.Component;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -20,7 +19,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -48,31 +46,15 @@ public class ModClient {
         }
         mcassetsDir = new File(dir, "assets");
 
-        // initialize langsIndexes
-        // get language indexes file
-        String content="";
-        try {
-            // Index file corresponding version query website:https://zh.minecraft.wiki/w/%E6%95%A3%E5%88%97%E8%B5%84%E6%BA%90%E6%96%87%E4%BB%B6?variant=zh
-            content = new String(
-                    Files.readAllBytes(
-                            new File(mcassetsDir, "indexes/17.json").toPath()
-                    ),
-                    StandardCharsets.UTF_8  // must specify utf-8.Because there may be error code.
-            );
-        } catch (IOException e) {
-            Mod.LOGGER.info(e.getMessage());
-        }
+        Gson gson = new Gson();
 
-        if(content.length() <= 13) {
-            Mod.LOGGER.info("index or indexList is empty");
-            langsIndex = new HashMap<>();
-        }
-        else{
-            langsIndex = new Gson().fromJson(
-                    content.substring(12,content.length()-1),
-                    new TypeToken<Map<String,Map<String,String>>>(){}.getType()
-            );
-        }
+        langsIndex = gson.fromJson(
+                gson.toJson(jsonToData(
+                        new File(mcassetsDir,"indexes/17.json"),
+                        new TypeToken<Map<String,Map<String,Map<String,String>>>>(){}
+                ).get("objects")),
+                new TypeToken<Map<String,Map<String,String>>>(){}.getType()
+        );
 
         // initialize defaultLangs
         InputStream inputStream = Mod.class.getResourceAsStream("/data/langs/en_us.json");  // get current version en_us.json
@@ -81,7 +63,7 @@ public class ModClient {
             defaultLangs = new HashMap<>();
         }
         else{
-            defaultLangs = new Gson().fromJson(new InputStreamReader(inputStream),new TypeToken<Map<String,String>>(){}.getType());
+            defaultLangs = gson.fromJson(new InputStreamReader(inputStream),new TypeToken<Map<String,String>>(){}.getType());
         }
         langs = defaultLangs;
     }
@@ -96,33 +78,42 @@ public class ModClient {
 
     private static void loadSecondLanguage(String selectLang) {
         if(selectLang.equals("en_us"))  langs = defaultLangs;
+        if(langsIndex == null)  return;
         if(langsIndex.get("minecraft/lang/" + selectLang + ".json")==null)  return;
 
-        String content="";
         String hashValue = langsIndex.get("minecraft/lang/" + selectLang + ".json").get("hash");
-        // get language file
-        try{
+        langs = jsonToData(
+                new File(mcassetsDir,"objects/" + hashValue.substring(0,2) + "/" + hashValue),
+                new TypeToken<Map<String,String>>(){}
+        );
+    }
+
+    /**
+     * Internally, It is Google's Gson implementation
+     *
+     * @param file - The target json file.
+     * @param typeToken - The type is you need type.
+     * @return - The type of typeToken You entered.
+     * @param <T>
+     */
+    private static <T> T jsonToData(File file, TypeToken<T> typeToken){
+        String content="";
+        try {
             content = new String(
-                    Files.readAllBytes(
-                            new File(
-                                    mcassetsDir,
-                                    "objects/" + hashValue.substring(0,2) + "/" + hashValue
-                            ).toPath()
-                    ),
+                    Files.readAllBytes(file.toPath()),
                     StandardCharsets.UTF_8  // must specify utf-8.Because there may be error code.
             );
         } catch (IOException e) {
             Mod.LOGGER.info("Failed to read lang file:" + e.getMessage());
         }
 
-        langs = new Gson().fromJson(
-                content,
-                new TypeToken<Map<String,String>>(){}.getType()
-        );
+        return new Gson().fromJson(content, typeToken);
     }
 
     @SubscribeEvent
     static void onItemTooltip(ItemTooltipEvent event) {
+        if(langs == null)   return;
+
         String transString = langs.get(event.getItemStack().getItem().getDescriptionId());
         if(transString==null || transString.isEmpty()) return;
 
